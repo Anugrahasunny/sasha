7ab467fc7207d664147127c72b7d03e5e9896d0c
Anugrahasunny <Anugrahasunny2006@gmail.com>
2025-04-03 12:08:41 +0530

    second push

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,j,rows;
+printf("enter the number of rows");
+scanf("%d",&rows);
+i<=rows;
+for(i=0;i<=rows;i++)
+{
+for(j=0;j<=i;j++)
+{
+printf("*");
+}
+printf("\n");
+}
+}
+
